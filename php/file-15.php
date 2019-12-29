53a3d841f7003259d288ea3f214c4daa4fa9416b
sakshamtaneja21 <sakshamtaneja7861@gmail.com>
2019-12-29 10:19:08 +0545

    php

---
+++ php/file-15.php
@@ -0,0 +1,16 @@
+<?php
+
+	//Armstrong (19-9-19)
+	$num = 153;		$total = 0;
+	$x = $num;
+	while($x != 0){
+		$rem = $x%10;
+		$total = ($total + ($rem * $rem * $rem));
+		$x = $x/10;
+	}
+	if($num == $total)
+		echo "An Armstrong number";
+	else
+		echo "Not an Armstrong number";
+
+?>
